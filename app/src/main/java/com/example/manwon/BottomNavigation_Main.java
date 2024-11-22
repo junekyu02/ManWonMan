10e6a44abe969b95e9b7f8ab2f621ad80de48b06
sanghyeon <8816089@naver.com>
2024-11-23 00:33:09 +0900

    bottomnavigationbar 추가 및 기본색상을 보라색->빨간색으로 변경 + HomeActivity를 HomeFragment.java로 변경

---
+++ app/src/main/java/com/example/manwon/BottomNavigation_Main.java
@@ -0,0 +1,62 @@
+package com.example.manwon;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.manwon.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class BottomNavigation_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if(itemId == R.id.home_icon){
+                    transferTo(HomeFragment.newInstance("param1","param2"));
+                    return true;
+                }
+                if(itemId == R.id.giftcard_icon){
+
+                }
+                if(itemId == R.id.feed_icon){
+
+                }
+                if(itemId == R.id.mypage_icon){
+
+                }
+                return false;
+            }
+        });
+
+        // 이미 선택되어있는데 또 누른 경우 fragment가 다시 초기화 되지 않고 아무일도 안하도록 하기 위함
+        bottomNavigationView.setOnItemReselectedListener(new NavigationBarView.OnItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+
+            }
+        });
+
+        // 처음 뜰 때는 Home이 뜨도록 함
+        transferTo(HomeFragment.newInstance("param1", "param2"));
+    }
+
+    // 동적으로 Fragment를 교체하기 위해 사용
+    // 즉, 해당 액티비티에서 특정 버튼을 클릭하면 다른 Fragment를 표시하도록 할 때 이 메서드를 사용.
+    private void transferTo(Fragment fragment){
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+}
---
+++ app/src/main/java/com/example/manwon/HomeFragment.java
@@ -0,0 +1,83 @@
+package com.example.manwon;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    // HomeFragment가 처음 생성될 때 호출되는 메서드
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Fragment의 레이아웃을 설정 (activity_home 레이아웃을 사용)
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // button1에 클릭 리스너 설정
+        ImageButton button1 = view.findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 물물교환 Activity로 이동
+            }
+        });
+
+        // button2에 클릭 리스너 설정
+        ImageButton button2 = view.findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // ItemRegistraionActivity로 이동
+                Intent intent = new Intent(getActivity(), ItemRegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // button3에 클릭 리스너 설정
+        ImageButton button3 = view.findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 둘러보기Activity로 이동
+            }
+        });
+
+        return view;
+    }
+}
